2a89e8eae8a96cf994dd6aecf565ed3dcad3dc71
owen <owenboutet@gmail.com>
2020-12-10 14:50:51 +0100

    max 5 pokemon par team

--- Team.py
+++ Team.py
@@ -6,7 +6,12 @@ class Team():
         self.pokemons = []
 
     def addPokemon(self, pokemon):
-        self.pokemons.append(pokemon)
+        if(len(self.pokemons) <=5):
+            self.pokemons.append(pokemon)
+            message = "pokémon ajouté"
+        else:
+            message = "équipe au grand complet, impossible d'en ajouter de nouveau"
+        return message
 
     def removePokemon(self, pokemon):
         self.pokemons.remove(pokemon)

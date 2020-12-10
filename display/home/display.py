4bf3c8c544f193d11ca38b472228ed987f9fdacd
corentin <corentin.fouquet@outlook.com>
2020-12-10 10:26:48 +0100

    import du sprite du pokemon

--- display/home/display.py
+++ display/home/display.py
@@ -1,20 +1,52 @@
-from PyQt5.QtWidgets import QMainWindow, QWidget
+import requests
+import urllib3
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QGridLayout
 from pokedex.display.setter import setter
 
 
 class HomeMenu(QMainWindow):
 
-    def __init__(self, json):
+    def __init__(self, pokemons):
         super().__init__()
-        self.initUI(json)
+        self.initUI(pokemons)
 
-    def initUI(self, json):
+    def initUI(self, pokemons):
 
-        self.setCentralWidget(display_home(json))
+        self.setCentralWidget(display_home(pokemons))
         setter(self)
 
 
-def display_home(json):
+def display_home(pokemons):
     w = QWidget()
+    print(pokemons)
+
+    # ----- elements label -----
+    name = QLabel(pokemons["name"])
+    id = QLabel("#" + str(pokemons["id"]))
+    sprite = sprite_pokemon(pokemons["url"], pokemons["name"])
+
+    # ----- grid -----
+    grid = QGridLayout()
+
+    grid.addWidget(sprite, 0, 0)
+    grid.addWidget(name, 1, 0)
+    grid.addWidget(id, 2, 0)
+
+    w.setLayout(grid)
 
     return w
+
+
+def sprite_pokemon(url, name):
+    name_file = "../sprites/" + name + ".png"
+    data = requests.get(url, allow_redirects=True)
+
+    open(name_file, 'wb').write(data.content)
+
+    image_label = QLabel()
+    image = QPixmap(name_file)
+
+    image_label.setPixmap(image)
+
+    return image_label
--- display/home/index.py
+++ display/home/index.py
@@ -2,10 +2,15 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from pokedex.display.home.display import HomeMenu
 
+pokemons = {
+    "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
+    "name": "bulbizar",
+    "id": 1
+}
 
 def main():
     app = QApplication(sys.argv)
-    w = HomeMenu()
+    w = HomeMenu(pokemons)
     sys.exit(app.exec_())
 
 

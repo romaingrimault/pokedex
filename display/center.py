ceb908924fd3dc5a73d7aafd3866c2020485eaf4
owen <owenboutet@gmail.com>
2020-12-10 15:17:04 +0100

    suppression des fichiers en double

--- display/center.py
+++
@@ -1,9 +0,0 @@
-from PyQt5.QtWidgets import QDesktopWidget
-
-
-def center(display):
-    # ----- center window -----
-    qr = display.frameGeometry()
-    center_position = QDesktopWidget().availableGeometry().center()
-    qr.moveCenter(center_position)
-    display.move(qr.topLeft())
--- display/setter.py
+++
@@ -1,9 +0,0 @@
-from pokedex.display.center import center
-
-
-def setter(display):
-    # ----- setter application (dimension, window title ...) -----
-    display.setGeometry(600, 600, 600, 400)
-    center(display)
-    display.setWindowTitle('Pokedex Kanto')
-    display.show()

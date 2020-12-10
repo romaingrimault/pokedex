77dfda798bdfa892d0caf71435098e5c061c32e6
corentin <corentin.fouquet@outlook.com>
2020-12-10 13:44:36 +0100

    ajout de la scrollbar

--- display/pokemonCharacter/index.py
+++ display/pokemonCharacter/index.py
@@ -2,8 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication
 from pokedex.display.pokemonCharacter.display import PokemonCharacter
 
-pokemon = [
-    {
+pokemon = {
         "url": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
         "name": "bulbizar",
         "id": 5,
@@ -11,7 +10,6 @@ pokemon = [
         "ability": ["overgrow", "chlorophyll"],
         "stats": [[45, "hp"], [45, "attack"], [45, "defense"], [45, "special-attack"], [45, "special-defense"], [45, "speed"]],
     }
-]
 
 
 def main():
